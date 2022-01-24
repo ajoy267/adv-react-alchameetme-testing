1b8746d5aa353de06b72879bb5039c9da0b11fe1
ajoy267 <andrew.joy12@hotmail.com>
2022-01-24 14:24:09 -0800

    passing app.test.jsx

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -1,4 +1,15 @@
+import { findByText, getByAltText, getByText, render, screen } from '@testing-library/react'
+import App from './App'
 
-test('Should render the header', () => {
+test.only('Should render the header', async () => {
+  render(<App />)
 
+  const logo = screen.getByAltText(/alchemy logo/i)
+  expect(logo).toBeInTheDocument()
+
+  const meet = screen.getByText(/meet/i)
+  expect(meet).toBeInTheDocument()
+
+  const name = await screen.findByText(/vonta/i)
+  expect(name).toBeInTheDocument()
 })
